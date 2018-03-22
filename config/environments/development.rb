a35ea3bbde59eeae41ad8d581dd475c9db993bf0
rajputakku <akankshasinghpatna29@gmail.com>
2018-03-22 12:16:44 +0530

    forgotpassword

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,24 @@
 Rails.application.configure do
+config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.perform_deliveries = true
+
+config.action_mailer.default :charset => "utf-8"
+
+config.action_mailer.smtp_settings = {
+address: "smtp.gmail.com",
+port: 587,
+domain: "gmail.com",
+authentication: "plain",
+enable_starttls_auto: true,
+user_name: "rtr.mohitsangwan@gmail.com",
+password: "mohitsingh"
+}
+
+
+
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -36,7 +56,7 @@ Rails.application.configure do
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
-
+  
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
