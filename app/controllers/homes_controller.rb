482f508c123a93a8496c0493d6bcca912c5764c3
Mohit211996 <rtr.mohitsangwan@gmail.com>
2018-03-23 19:20:14 +0530

    frontend_complete

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,7 @@
 class HomesController < ApplicationController
 	def index
+		@startup_count=User.where(role_id: '1').count
+		@client_count=User.where(role_id: '2').count
+		@all = User.all.count
 	end
 end
