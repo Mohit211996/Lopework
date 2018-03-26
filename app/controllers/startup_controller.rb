1cbdc8cde35d98adca4d968f8184308eb21f7043
rajputakku <akankshasinghpatna29@gmail.com>
2018-03-26 14:09:21 +0530

    showproject

--- app/controllers/startup_controller.rb
+++ app/controllers/startup_controller.rb
@@ -1,9 +1,17 @@
 class StartupController < ApplicationController
 	def index
+
 		@projects = Project.all
+
+
 	end
 	def project_status
 		@project_status = Project_status.find(params[:id])
+	end
+	def project_detail
+		@project = Project.all.first
+
+
 	end
 	
 end
