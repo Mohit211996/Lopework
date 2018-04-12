b8b1d9cca99c162e8591cb8e1294beef25d629a7
rajputakku <akankshasinghpatna29@gmail.com>
2018-04-12 11:27:21 +0530

    css

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -36,7 +36,7 @@ end
  @bid = Bid.new(bid_params)
     respond_to do |format|
       if @bid.save
-        format.html { redirect_to bids_url , notice: 'Bid was successfully created.' }
+        format.html { redirect_to startup_url , notice: 'Bid was successfully created.' }
         format.json { render :show, status: :created, location: @bids }
       else
         format.html { render :new }
